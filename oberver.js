4c54a55c938491380fd521edc0f1657f07d2d807
jpesg <42676743+jpesg@users.noreply.github.com>
2021-02-01 08:56:31 +0000

    Create oberver.js

---
+++ oberver.js
@@ -0,0 +1,77 @@
+const EventEmitter = require('events').EventEmitter
+
+const customEE = new EventEmitter();
+
+let count = 0;
+setInterval(() => {
+    customEE.emit('ping', count);
+    count++;
+}, 3000);
+
+customEE.once('ping', () => console.log('started pinging'))
+    .on('ping', num => console.log(`ping #${num} from module`));
+
+const fs = require('fs');
+// Manages a Subject interactions with observer
+class SubjectManager {
+    constructor() {
+        this._observers = {};
+    }
+    addObserver(eventType, observer) {
+        if(!this._observers[eventType]) {
+            this._observers[eventType] = [];
+        }
+        this._observers[eventType].push(observer);
+    }
+    removeObserver(eventType, observer) {
+        const idx = this._observers[eventType].indexOf(observer);
+        if(idx > -1){
+            this._observers[eventType].splice(idx);
+        }
+    }
+    notify(eventType, data) {
+        this._observers[eventType].forEach(observer => observer.update(data));
+    }
+}
+
+class FileManager {
+    constructor() {
+        this.subjectManager = new SubjectManager();
+    }
+    monitorFile(path) {
+        // eslint-disable-next-line node/no-unsupported-features/es-syntax
+        fs.watch(path, (data) => {
+            return this.subjectManager.notify("change", {path, data})
+        });
+    }
+    addObserver(eventType, observer) {
+        this.subjectManager.addObserver(eventType, observer);
+    }
+    removeObserver(eventType, observer) {
+        this.subjectManager.removeObserver(eventType, observer);
+    }
+}
+class LoggingObserver {
+    update({ data }) {
+        console.log(data);
+    }
+}
+class SizeChangeObserver {
+    constructor() {
+        this.size = 0;
+    }
+    update({ path }) {
+        const newSize = fs.statSync(path).size;
+        if(newSize !== this.size) {
+            console.log("size increase to "  + newSize);
+            this.size = newSize;
+        }
+    }
+}
+
+// Subject class//
+const fileManager = new FileManager();
+// Adding observers
+fileManager.addObserver("change", new LoggingObserver());
+fileManager.addObserver("change", new SizeChangeObserver());
+fileManager.monitorFile(process.argv[1]);
